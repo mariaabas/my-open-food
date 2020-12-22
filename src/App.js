a5513e090b62aa3eff10d1ff354428347071cc94
maria.abas <maria.abas@upcnet.es>
2020-12-22 23:31:49 +0100

    Add call api open foods

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import Container from '@material-ui/core/Container';
+import { makeStyles, Container, Grid} from '@material-ui/core';
 import BarCode from './components/BarCode.js';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  container: {
+    margin: '0 auto',
+    display: 'flex',
+    justifyContent: 'center',
+    minHeight: '100vh',
+    flexDirection: 'column'
+  }
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      {/*<header className="App-header">
+    <div className={classes.root}>
+{/*      {<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,11 +34,12 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>*/}
-      <Container>
-          <BarCode />
-      </Container>
+        </a>}
+      </header> }*/}
+{        <Container className={classes.container}>
+              <BarCode />
+          </Container> }
+
     </div>
   );
 }
--- src/components/BarCode.js
+++ src/components/BarCode.js
@@ -1,10 +1,35 @@
 import React from 'react'
-import TextField from "@material-ui/core/TextField";
+import { TextField, Grid, Button } from "@material-ui/core";
+import Search from '@material-ui/icons/Search';
+import axios from "axios";
+
 
 function BarCode() {
 
+    const url = " https://world.openfoodfacts.org/api/v0/product/"
+    const textButton = 'Search';
+    const textError = 'Only numbers'
+    const [ codiDeBarres, setCodiDeBarres ] = React.useState('');
+    const error = codiDeBarres === /^[0-9\b]+$/;
+
+    function cridaApi(code) {
+        axios.get(url + code +'.json')
+            .then(resposta => console.log(resposta))
+            .catch((error) => console.log(error))
+    }
+
     return (
-        <TextField id="bar-code" label="BarCode" variant="outlined"/>
+        <Grid>
+            <TextField id="bar-code"
+                       label="BarCode"
+                       variant="outlined"
+                       value={codiDeBarres}
+                       onChange={(code) => setCodiDeBarres(code.target.value)}
+                       helperText={textError}
+                       error={error}
+            ></TextField>
+        <Button variant="contained" color="primary" onClick={() => cridaApi(codiDeBarres)}>{textButton}</Button>
+        </Grid>
     )
 }
 
